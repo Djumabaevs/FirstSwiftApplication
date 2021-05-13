ddf8447cfbe1168d6b0d90f332d2edcade426e33
Djumabaevs <76224728+Djumabaevs@users.noreply.github.com>
2021-05-13 09:39:30 +0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirstSwiftApplication
+//
+//  Created by Bakyt Dzhumabaev on 13/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("hello")
+    
+        
+        
+    }
+
+
+}
+
